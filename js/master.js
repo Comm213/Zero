25dfde2240c452c29ffef458e48ed12decb2b4fc
Comm213 <you@example.com>
2021-08-25 03:23:29 +0300

    Animate Skills Section

--- js/master.js
+++ js/master.js
@@ -131,3 +131,31 @@ function randomizeImgs() {
     }, 1000);
   }
 }
+
+randomizeImgs();
+
+// Select Skills Selector
+let ourSkills = document.querySelector(".skills");
+
+window.onscroll = function () {
+  // Skills Offset Top
+  let skillsOffsetTop = ourSkills.offsetTop;
+
+  // Skills Outer Height
+  let skillsOuterHeight = ourSkills.offsetHeight;
+
+  // Window Height
+  let windowHeight = this.innerHeight;
+
+  // Window ScrollTop
+  let windowsScrollTop = this.pageYOffset;
+
+  if (windowsScrollTop > skillsOffsetTop + skillsOuterHeight - windowHeight) {
+    let allSkills = document.querySelectorAll(
+      ".skill-box .skill-progress span"
+    );
+    allSkills.forEach((skill) => {
+      skill.style.width = skill.dataset.progress;
+    });
+  }
+};
