63191e4bb1ef433db7d26643eb28ad4c67c7cab5
Comm213 <hmahfouz705@gmail.com>
2021-09-11 09:23:38 +0300

    Code Enhacment

--- js/master.js
+++ js/master.js
@@ -2,22 +2,22 @@
 let mainColors = localStorage.getItem("color_option");
 
 if (localStorage !== null) {
-  // console.log("Local Storage Is Not Empty You Can Set It On Root Now");
-  // console.log(localStorage.getItem("color_option"));
-  document.documentElement.style.setProperty(
-    "--main--color",
-    localStorage.getItem("color_option")
-  );
-  // Remove Active Class From All Colors List Items
-  document.querySelectorAll(".colors-list li").forEach((element) => {
-    element.classList.remove("active");
+    // console.log("Local Storage Is Not Empty You Can Set It On Root Now");
+    // console.log(localStorage.getItem("color_option"));
+    document.documentElement.style.setProperty(
+        "--main--color",
+        localStorage.getItem("color_option")
+    );
+    // Remove Active Class From All Colors List Items
+    document.querySelectorAll(".colors-list li").forEach((element) => {
+        element.classList.remove("active");
 
-    // Add Active Class On Element With Data-Color === Local Storage Item
-    if (element.dataset.color === mainColors) {
-      // Add Active Class
-      element.classList.add("active");
-    }
-  });
+        // Add Active Class On Element With Data-Color === Local Storage Item
+        if (element.dataset.color === mainColors) {
+            // Add Active Class
+            element.classList.add("active");
+        }
+    });
 }
 //Random Background Option
 let backgroundOption = true;
@@ -30,30 +30,30 @@ let backgroundLocalItem = localStorage.getItem("background_option");
 
 // Check If Random Background Local Storage Is Not Empty
 if (backgroundLocalItem !== null) {
-  if (backgroundLocalItem === "true") {
-    backgroundOption = true;
-  } else {
-    backgroundOption = false;
-  }
-  // Remove Active class From All Spans
-  document.querySelectorAll(".random-backgrounds span").forEach((element) => {
-    element.classList.remove("active");
-  });
+    if (backgroundLocalItem === "true") {
+        backgroundOption = true;
+    } else {
+        backgroundOption = false;
+    }
+    // Remove Active class From All Spans
+    document.querySelectorAll(".random-backgrounds span").forEach((element) => {
+        element.classList.remove("active");
+    });
 
-  if (backgroundLocalItem === "true") {
-    document.querySelector(".random-backgrounds .yes").classList.add("active");
-  } else {
-    document.querySelector(".random-backgrounds .no").classList.add("active");
-  }
+    if (backgroundLocalItem === "true") {
+        document.querySelector(".random-backgrounds .yes").classList.add("active");
+    } else {
+        document.querySelector(".random-backgrounds .no").classList.add("active");
+    }
 }
 
 // Toggle Spin Class On Icon
 document.querySelector(".toggle-settings .fa-gear").onclick = function () {
-  //Toggle Class Fa-Spin For Rotation on Self
-  this.classList.toggle("fa-spin");
+    //Toggle Class Fa-Spin For Rotation on Self
+    this.classList.toggle("fa-spin");
 
-  //Toggle Class Open ON Main Settings Box
-  document.querySelector(".settings-box").classList.toggle("open");
+    //Toggle Class Open ON Main Settings Box
+    document.querySelector(".settings-box").classList.toggle("open");
 };
 
 // Switch Colors
@@ -61,53 +61,43 @@ const colorsLi = document.querySelectorAll(".colors-list li");
 
 // Loop On All List Items
 colorsLi.forEach((li) => {
-  li.addEventListener("click", (e) => {
+
     //Click On Every List Items
-    console.log(e.target.dataset.color);
+    li.addEventListener("click", (e) => {
 
-    //Set Color On Root
-    document.documentElement.style.setProperty(
-      "--main--color",
-      e.target.dataset.color
-    );
-    // Set Color On Local Storage
-    localStorage.setItem("color_option", e.target.dataset.color);
+        //Set Color On Root
+        document.documentElement.style.setProperty(
+            "--main--color",
+            e.target.dataset.color
+        );
+        // Set Color On Local Storage
+        localStorage.setItem("color_option", e.target.dataset.color);
 
-    // Remove Active Class From All Children
-    e.target.parentElement.querySelectorAll(".active").forEach((element) => {
-      element.classList.remove("active");
+
     });
-    //Add Active Class On Self
-    e.target.classList.add("active");
-  });
 });
 // Switch Random Background Option
 const randomBackEl = document.querySelectorAll(".random-backgrounds span");
 
 // Loop On All Spans
 randomBackEl.forEach((span) => {
-  span.addEventListener("click", (e) => {
+
     //Click On Every Span
-    console.log(e.target.dataset.color);
+    span.addEventListener("click", (e) => {
 
-    // Remove Active Class From All Children
-    e.target.parentElement.querySelectorAll(".active").forEach((element) => {
-      element.classList.remove("active");
+        handleActive(e);
+
+        if (e.target.dataset.background === "yes") {
+            backgroundOption = true;
+            randomizeImgs();
+
+            localStorage.setItem("background_option", true);
+        } else {
+            backgroundOption = false;
+            clearInterval(backgroundInterval);
+            localStorage.setItem("background_option", false);
+        }
     });
-    //Add Active Class On Self
-    e.target.classList.add("active");
-
-    if (e.target.dataset.background === "yes") {
-      backgroundOption = true;
-      randomizeImgs();
-
-      localStorage.setItem("background_option", true);
-    } else {
-      backgroundOption = false;
-      clearInterval(backgroundInterval);
-      localStorage.setItem("background_option", false);
-    }
-  });
 });
 // Select Landing page Element
 
@@ -119,17 +109,17 @@ let imgsArray = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
 
 //Function To Randomize Imgs
 function randomizeImgs() {
-  if (backgroundOption === true) {
-    backgroundInterval = setInterval(() => {
-      // Get Random Number
-      let randomNumber = Math.floor(Math.random() * imgsArray.length);
+    if (backgroundOption === true) {
+        backgroundInterval = setInterval(() => {
+            // Get Random Number
+            let randomNumber = Math.floor(Math.random() * imgsArray.length);
 
-      // Change background image URL
+            // Change background image URL
 
-      landingPage.style.backgroundImage =
-        'url("imgs/' + imgsArray[randomNumber] + '")';
-    }, 1000);
-  }
+            landingPage.style.backgroundImage =
+                'url("imgs/' + imgsArray[randomNumber] + '")';
+        }, 1000);
+    }
 }
 
 randomizeImgs();
@@ -138,26 +128,26 @@ randomizeImgs();
 let ourSkills = document.querySelector(".skills");
 
 window.onscroll = function () {
-  // Skills Offset Top
-  let skillsOffsetTop = ourSkills.offsetTop;
+    // Skills Offset Top
+    let skillsOffsetTop = ourSkills.offsetTop;
 
-  // Skills Outer Height
-  let skillsOuterHeight = ourSkills.offsetHeight;
+    // Skills Outer Height
+    let skillsOuterHeight = ourSkills.offsetHeight;
 
-  // Window Height
-  let windowHeight = this.innerHeight;
+    // Window Height
+    let windowHeight = this.innerHeight;
 
-  // Window ScrollTop
-  let windowsScrollTop = this.pageYOffset;
+    // Window ScrollTop
+    let windowsScrollTop = this.pageYOffset;
 
-  if (windowsScrollTop > skillsOffsetTop + skillsOuterHeight - windowHeight) {
-    let allSkills = document.querySelectorAll(
-      ".skill-box .skill-progress span"
-    );
-    allSkills.forEach((skill) => {
-      skill.style.width = skill.dataset.progress;
-    });
-  }
+    if (windowsScrollTop > skillsOffsetTop + skillsOuterHeight - windowHeight) {
+        let allSkills = document.querySelectorAll(
+            ".skill-box .skill-progress span"
+        );
+        allSkills.forEach((skill) => {
+            skill.style.width = skill.dataset.progress;
+        });
+    }
 };
 
 // Create PopUp With The Image
@@ -165,73 +155,123 @@ window.onscroll = function () {
 let ourGallery = document.querySelectorAll(".gallery img");
 
 ourGallery.forEach((img) => {
-  img.addEventListener("click", (e) => {
-    // Create Overlay Element
-    let overlay = document.createElement("div");
+    img.addEventListener("click", (e) => {
+        // Create Overlay Element
+        let overlay = document.createElement("div");
 
-    // Add Class To Overlay
-    overlay.className = "popup-overlay";
+        // Add Class To Overlay
+        overlay.className = "popup-overlay";
 
-    // Append Overlay To The Body
-    document.body.appendChild(overlay);
+        // Append Overlay To The Body
+        document.body.appendChild(overlay);
 
-    //Create The Popup
-    let popupBox = document.createElement("div");
+        //Create The Popup
+        let popupBox = document.createElement("div");
 
-    // Add Class To The Popup Box
-    popupBox.className = "popup-box";
+        // Add Class To The Popup Box
+        popupBox.className = "popup-box";
 
-    if (img.alt !== null) {
-      // Create Heading
-      let imgHeading = document.createElement("h3");
+        if (img.alt !== null) {
+            // Create Heading
+            let imgHeading = document.createElement("h3");
 
-      // Create text for Heading
-      let imgText = document.createTextNode(img.alt);
+            // Create text for Heading
+            let imgText = document.createTextNode(img.alt);
 
-      // Append The Text To The Heading
-      imgHeading.appendChild(imgText);
+            // Append The Text To The Heading
+            imgHeading.appendChild(imgText);
 
-      // Append The Heading To The Popup Box
-      popupBox.appendChild(imgHeading);
-    }
+            // Append The Heading To The Popup Box
+            popupBox.appendChild(imgHeading);
+        }
 
-    //Create The Image
-    let popupImage = document.createElement("img");
+        //Create The Image
+        let popupImage = document.createElement("img");
 
-    // Set Image Source
-    popupImage.src = img.src;
+        // Set Image Source
+        popupImage.src = img.src;
 
-    // Add Image To Popup Box
-    popupBox.appendChild(popupImage);
+        // Add Image To Popup Box
+        popupBox.appendChild(popupImage);
 
-    // Append Popup Box To Body
-    document.body.appendChild(popupBox);
+        // Append Popup Box To Body
+        document.body.appendChild(popupBox);
 
-    // Create The Close Span
+        // Create The Close Span
 
-    let closeButton = document.createElement("span");
+        let closeButton = document.createElement("span");
 
-    // Create The Close Button Text
-    let closeButtonText = document.createTextNode("X");
+        // Create The Close Button Text
+        let closeButtonText = document.createTextNode("X");
 
-    // Append Text To Close Button
-    closeButton.appendChild(closeButtonText);
+        // Append Text To Close Button
+        closeButton.appendChild(closeButtonText);
 
-    // Add Class To Close Button
-    closeButton.className = "close-button";
+        // Add Class To Close Button
+        closeButton.className = "close-button";
 
-    // Add Close Button To The Popup Box
-    popupBox.appendChild(closeButton);
-  });
+        // Add Close Button To The Popup Box
+        popupBox.appendChild(closeButton);
+    });
 });
 
 // Close Popup
 document.addEventListener("click", function (e) {
-  if (e.target.className == "close-button") {
-    // Remove The Current Popup
-    e.target.parentNode.remove();
+    if (e.target.className == "close-button") {
+        // Remove The Current Popup
+        e.target.parentNode.remove();
 
-    // Remove Overlay
-    document.querySelector(".popup-overlay").remove();
-  }
+        // Remove Overlay
+        document.querySelector(".popup-overlay").remove();
+    }
 });
+
+// Select All Bullets
+const allBullets = document.querySelectorAll(".nav-bullets .bullets");
+allBullets.forEach(bullet => {
+    bullet.addEventListener("click", (e) => {
+        document.querySelector(e.target.dataset.section).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Select All Links
+const allLinks = document.querySelectorAll(".links a");
+allLinks.forEach(link => {
+    link.addEventListener("click", (e) => {
+
+        e.preventDefault();
+
+        document.querySelector(e.target.dataset.section).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+function scrollToSomewhere(elements) {
+    allLinks.forEach(ele => {
+        ele.addEventListener("click", (e) => {
+
+            e.preventDefault();
+
+            document.querySelector(e.target.dataset.section).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+}
+
+scrollToSomewhere(allBullets);
+scrollToSomewhere(allLinks);
+
+// Handle Active State
+function handleActive(ev){
+    // Remove Active Class From All Children
+    ev.target.parentElement.querySelectorAll(".active").forEach((element) => {
+        element.classList.remove("active");
+    });
+    //Add Active Class On Self
+    ev.target.classList.add("active");
+}
